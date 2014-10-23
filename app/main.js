f8e57a569ac2b86f226e81644a99678b40966277
thingalon <thingalon@gmail.com>
2014-10-23 20:17:04 +1100

    Define my own menu, allowing cmd+q and cmd+o to work. Still ugly test code.

--- app/main.js
+++ app/main.js
@@ -4,11 +4,13 @@ var Job = require( './job.js' );
 
 require('crash-reporter').start();
 
+//	On window close, quit.
 app.on( 'window-all-closed', function() {
 	if ( process.platform != 'darwin' )
 		app.quit();
 } );
 
+//	Create the main window, load the main UI
 var mainWindow = null;
 app.on( 'ready', function() {
 	mainWindow = new BrowserWindow( {
