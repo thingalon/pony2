8d47c2756fee833be8cc80e9a7086f30451ddcb6
thingalon <thingalon@gmail.com>
2014-12-02 11:06:25 +1100

    Implement cut/copy/paste

--- app/ui/src/menu.jsx
+++ app/ui/src/menu.jsx
@@ -46,6 +46,25 @@ var mainMenuTemplate = [
 			},
 			{ label: 'Do other stuff' },
 		],
-	}
+	}, {
+		label: 'Edit',
+		submenu: [
+      		{
+        		label: 'Cut',
+        		accelerator: 'Command+X',
+        		selector: 'cut:'
+      		},
+      		{
+        		label: 'Copy',
+        		accelerator: 'Command+C',
+        		selector: 'copy:'
+      		},
+      		{
+        		label: 'Paste',
+        		accelerator: 'Command+V',
+        		selector: 'paste:'
+      		},
+		],
+	},
 ];
 
